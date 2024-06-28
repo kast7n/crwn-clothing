97791c3cf1e4bce4e5b39dc7d378b825d1c3d956
Ali Rida Darsa <Milotheapache@gmail.com>
2024-06-28 14:46:21 +0300

    fix imgUrl error

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -3,14 +3,16 @@ import {
   Body,
   BackgroundImage,
 } from "./directory-item.styles";
+import { useNavigate } from "react-router-dom";
 
 const DirectoryItem = ({ category }) => {
-  const { title, imageUrl } = category;
-
+  const { title, imageUrl, route } = category;
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       {" "}
-      <BackgroundImage imageUrl={imageUrl} />
+      <BackgroundImage imageurl={imageUrl} />
       <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
